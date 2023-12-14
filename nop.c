78663681f549622516a7f770b8aa179c93efdffd
Erdey <erdeysyoum@gmail.com>
2023-12-14 07:57:10 -0800

    nop created

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - perform nathing
+ * @stack: the stack
+ * @line_number: from where we call the function
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
